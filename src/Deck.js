59052d20e7207d0a56b9e66c32f23283045cfe60
gsingh737 <ssgurpreetsingh@gmail.com>
2017-08-02 23:48:42 -0400

    Initial boilerplate with some animation plaground

---
+++ src/Deck.js
@@ -0,0 +1,23 @@
+/**
+ * Created by User on 8/2/2017.
+ */
+import React, {Component} from 'react';
+import {View, Animated} from 'react-native';
+
+class Deck extends Component {
+    renderCards(){
+        return this.props.data.map(item => {
+            return this.props.renderCard(item);
+        });
+    }
+    render(){
+        return (
+            <View>
+                {this.renderCards()}
+            </View>
+        );
+
+    }
+}
+
+export default Deck;
