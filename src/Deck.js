16ffb953b719ce2268076e00906a7c8b58930fa6
gsingh737 <ssgurpreetsingh@gmail.com>
2017-08-03 22:25:20 -0400

    force swipe

--- src/Deck.js
+++ src/Deck.js
@@ -10,6 +10,9 @@ import {
 } from 'react-native';
 
 const SCREEN_WIDTH = Dimensions.get('window').width;
+const SWIPE_THRESHOLD = 0.25 * SCREEN_WIDTH;
+const SWIPE_OUT_DURATION = 250;
+
 class Deck extends Component {
     constructor(props) {
         super(props);
@@ -19,12 +22,28 @@ class Deck extends Component {
             onPanResponderMove: (event, gesture) => {
                 position.setValue({x: gesture.dx, y: gesture.dy });
             },
-            onPanResponderRelease: () => {
-                this.resetPosition();
+            onPanResponderRelease: (event, gesture) => {
+                console.log(gesture.dx);
+                console.log(SWIPE_THRESHOLD);
+                if(gesture.dx > SWIPE_THRESHOLD){
+                    this.forceSwipe('right');
+                } else if(gesture.dx < -SWIPE_THRESHOLD){
+                    this.forceSwipe('left');
+                } else {
+                    this.resetPosition();
+                }
             }
         });
 
         this.state = { panResponder, position };
+    }
+
+    forceSwipe(direction){
+        const x = direction === 'right' ? SCREEN_WIDTH : -SCREEN_WIDTH;
+        Animated.timing(this.state.position, {
+            toValue: {x, y: 0},
+            duration: SWIPE_OUT_DURATION
+        }).start();
     }
 
     resetPosition() {
